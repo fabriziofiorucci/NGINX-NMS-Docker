2c64e66c97cb62fb6d6a71da98f55540fce209b4
Fabrizio Fiorucci <fiorucci@oasi.asti.it>
2023-07-24 20:23:30 +0200

    Tested with NGINX Instance Manager 2.12.0 and Security Monitoring 1.6.0

--- scripts/buildNIM.sh
+++ scripts/buildNIM.sh
@@ -19,7 +19,7 @@ Automated build:\n\n
 -K [file.key]\t\t- Key file to pull packages from the official NGINX repository\n
 -A\t\t\t- Enable API Connectivity Manager - optional\n
 -W\t\t\t- Enable Security Monitoring - optional\n
--P [version]\t\t- Enable WAF policy compiler, version can be any [v3.1088.2|v4.2.0|v4.100.1|v4.218.0|v4.279.0] - optional\n
+-P [version]\t\t- Enable WAF policy compiler, version can be any [v3.1088.2|v4.2.0|v4.100.1|v4.218.0|v4.279.0|v4.402.0] - optional\n
 -D\t\t\t- Enable App Delivery Manager - optional\n\n
 === Examples:\n\n
 Manual build:\n
